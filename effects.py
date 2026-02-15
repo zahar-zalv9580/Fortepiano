e822dae9c0d1e2efc85213cb5f21204d2888f931
zahar-zalv9580 <zaharzalvov@gmail.com>
2026-02-15 14:51:23 +0200

    aaa

--- effects.py
+++ effects.py
@@ -1,6 +1,39 @@
 from pygame import *
 from settings import BLACK
 
+C_IMG = transform.scale(image.load("assets/images/notes/c.png"), (50, 50))
+D_IMG = transform.scale(image.load("assets/images/notes/d.png"), (50, 50))
+E_IMG = transform.scale(image.load("assets/images/notes/e.png"), (50, 50))
+
+NOTE_IMAGES = {
+    "C": C_IMG,
+    "D": D_IMG,
+    "E": E_IMG
+}
+
+FLYING_NOTES = []
+
+def spawn_flying_notes(rect, note_name:str | None):
+    if not note_name:
+        return
+    img = NOTE_IMAGES.get(note_name)
+    if not img:
+        return
+    x = rect.centerx - img.get_width() // 2
+    y = rect.top - img.get_height() - 10
+    FLYING_NOTES.append({"image": img, "x": x, "y": y, "vy": -1})
+
+
+    def update_and_draw_flying_notes(screen):
+        to_remove = []
+        for n in FLYING_NOTES:
+            n["y"] += n["vy"]
+            screen.blit(n["img"], (n["x"], n["y"]))
+            if n["y"] + n["img"].get_height() < 0:
+                to_remove.append(n)
+        for n in to_remove:
+            FLYING_NOTES.remove(n)
+
 def draw_key_effect(screen, rect, is_pressed=False):
     if not is_pressed:
         base_color = (220, 220, 220)
--- keys.py
+++ keys.py
@@ -1,10 +1,28 @@
 from pygame import Rect
-from effects import draw_key_effect
+from effects import spawn_flying_notes, update_and_draw_flying_notes
+
+
+KEY_UNPRESSED = transform.scale(image.load("assets/images/key_unpressed.png"), (100, 250))
+NOTE_BY_INDEX = {
+    0: "C",
+    1: "D",
+    2: "E",
+}
+
+PREV_PRESSED = set()
+
 
 def draw_keys(screen, key_rects, pressed_keys):
+    global PREV_PRESSED
+    pressed_set = set(pressed_keys)
+    
     for i, rect in enumerate(key_rects):
         is_pressed = i in pressed_keys
-        draw_key_effect(screen, rect, is_pressed)
+        screen.blit(KEY_UNPRESSED, (rect.x, rect.y))
+        if is_pressed and i not in PREV_PRESSED:
+            spawn_flying_notes(rect, NOTE_BY_INDEX.get(i))
+        PREV_PRESSED = pressed_set
+        update_and_draw_flying_notes(screen)
 
 def create_key_rects(num_keys, start_x=50, start_y=100, key_width=100, key_height=250):
     rects = []
